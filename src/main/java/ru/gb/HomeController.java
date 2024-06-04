6872431ef4a84b3739995df263fc48dc362b6010
Yauheni Aviltsau <xopek-007@yandex.ru>
2024-06-04 07:55:32 +0300

    init+comments

--- src/main/java/ru/gb/HomeController.java
+++ src/main/java/ru/gb/HomeController.java
@@ -1,5 +1,6 @@
 package ru.gb;
 
+import lombok.extern.slf4j.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Slf4j
 @Controller
 public class HomeController {
 
@@ -24,6 +25,9 @@ public class HomeController {
 
   @GetMapping("/list")
   public String list(@RequestParam int count, Model model) {
+
+    // Образец вызова:
+    //    http://localhost:8080/list?count=8 - выведет 8 записей
     List<Item> items = new ArrayList<>();
     for (int i = 1; i <= count; i++) {
       items.add(new Item(i, "Item #" + i));
@@ -35,6 +39,12 @@ public class HomeController {
 
   @GetMapping("/home")
   public String home(@RequestParam(required = false) String name, @RequestParam(required = false) String color, Model model) {
+//    Если в строке браузера ввести: 'http://localhost:8080/home -
+//            появится заголовок "Hello, World!" черного (по умолчанию) цвета
+//    Если в строке браузера ввести: 'http://localhost:8080/home?name=Jack' -
+//            появится заголовок "Hello, Jack!" черного (по умолчанию) цвета
+//    Если в строке браузера ввести: 'http://localhost:8080/home?name=Jack&color=red' -
+//            появится заголовок "Hello, Jack!" красного цвета
     if (name != null) {
       model.addAttribute("name", name);
     } else {
